cfb386a4f4fc2f5c61f4a183e19d84e64bb8b012
prajwal1014 <prajwalpg04@gmail.com>
2025-02-28 15:15:20 +0530

    output block

---
+++ output_block.tf
@@ -0,0 +1,18 @@
+provider "aws" {
+  region     = "ap-south-1"
+  access_key = "---------------------"
+  secret_key = "---------------------------"
+}
+
+resource "aws_instance" "ex1" {
+  ami           = "ami-00bb6a80f01f03502"
+  instance_type = "t2.micro"
+  key_name      = "n2025"
+  tags = {
+    Name = "marcos"
+  }
+}
+
+output "public_ip" {
+  value = aws_instance.ex1.public_ip
+}
